752d4387670919508d2dcf816350ffbffede3245
johntrimis <johntrimis@gmail.com>
2011-05-05 01:47:38 -0400

    Initial Check-in.
    V1.0

---
+++ Splitter/NameValuePair.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Splitter
+{
+    /// <summary>
+    ///     A container for a set of NameValue Pairs.  Can parse this out of a properly formatted string
+    /// </summary>
+    public class NameValuePair : System.Collections.IEnumerable
+    {
+        private Dictionary<string, string> _DataStore;
+        /// <summary>
+        ///     Create a new NameValuePair object based on the RawText
+        /// </summary>
+        /// <param name="RawText">
+        ///     String in the format of:
+        ///     Attribute1=value Attribute2="value 2" Attribute3=value3
+        /// </param>
+        public NameValuePair(string RawText)
+        {
+            this._DataStore = new Dictionary<string, string>();
+
+            List<string> commands = SmartSplit(RawText, " ");
+            foreach (string thisCmd in commands)
+            {
+                List<string> namevalue = SmartSplit(thisCmd, "=");
+
+                if (namevalue.Count > 1)
+                {
+                    this[namevalue[0]] = namevalue[1];
+                }
+            }
+
+
+        }
+
+        /// <summary>
+        ///     Gets or sets a specific value from the this NameValuePair
+        ///     Note: Key and value are both case insensitive
+        /// </summary>
+        /// <param name="Indexer">
+        ///     A key that the value is indexed under
+        /// </param>
+        /// <returns>
+        ///     The value (all uppercase) or string.Empty if no match is found.
+        /// </returns>
+        public string this[string Indexer]
+        {   // Indexer
+            get
+            {   // GETTER
+                if (this._DataStore.ContainsKey(Indexer.ToUpper()))
+                {   // Exist
+                    return this._DataStore[Indexer.ToUpper()];
+                }   // Exists
+                else
+                {   // Blank
+                    return string.Empty;
+                }   // Blank
+            }   // GETTER
+            set
+            {   // SETTER
+                if (this._DataStore.ContainsKey(Indexer.ToUpper()))
+                {   // Update
+                    this._DataStore[Indexer.ToUpper()] = value.ToUpper();
+                }   // Update
+                else
+                {   // Add
+                    this._DataStore.Add(Indexer.ToUpper(), value.ToUpper());
+                }   // Add
+            }   // SETTER
+        }   // Indexer
+
+        /// <summary>
+        ///     Perform a smart split, don't split when inside quotes.  Leave quoted data in tact
+        /// </summary>
+        /// <param name="RawText">
+        ///     The Raw string to search through and split out.
+        /// </param>
+        /// <param name="Delimiters">
+        ///     The Delimiter to use in the split
+        /// </param>
+        /// <returns>
+        ///     An collection of strings containing all the string values split out
+        /// </returns>
+        private List<string> SmartSplit(string RawText, string Delimiters)
+        {
+            List<string> output = new List<string>();
+
+            string Quotes = "\"\'";
+            string Segment = string.Empty;
+            bool Parse = true;
+
+            foreach (char thisChar in RawText)
+            {	//<StepThroughAllCharsInString>
+                if (Parse)
+                {	//<OutSideOfQuotes>
+                    if (Delimiters.IndexOf(thisChar) != -1)
+                    {	//<DelimiterChar>
+                        output.Add(Segment);
+                        Segment = string.Empty;
+                    }	//</DelimiterChar>
+                    else
+                    {	//<NonDelimiterChar>
+                        if (Quotes.IndexOf(thisChar) != -1)
+                        {	//<FoundQuote>
+                            Parse = false;
+                        }	//</FoundQuote>
+                        else
+                        {	//<NonQuoteChar>
+                            Segment += thisChar;
+                        }	//</NonQuoteChar>
+                    }	//</NonDelimiterChar>
+                }	//</OutSideOfQuotes>
+                else
+                {	//<InsideQuotes>
+                    if (Quotes.IndexOf(thisChar) != -1)
+                    {	//<FoundQuote>
+                        Parse = true;
+                    }	//</FoundQuote>
+                    else
+                    {	//<NonQuoteChar>
+                        Segment += thisChar;
+                    }	//</NonQuoteChar>
+                }	//</InsideQuotes>
+            }	//</StepThroughAllCharsInString>
+            //if (endofline == true && Segment == )
+
+            if (Segment != string.Empty) output.Add(Segment);
+            else output.Add("");
+            // the else is necessary because this indicates that the segment is blank.  for cases where the last column was blank, this was causing the
+            //  segment list to have 1 less entry (missing the last blank column) and result in an invalid index error when trying to access the expected column.
+            //  Again, this was only applicable when the user is allowing blank columns and filling them in with a default value.
+
+
+            return output;
+        }
+
+
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            return this._DataStore.GetEnumerator();
+        }
+    }
+}
---
+++ Splitter/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Splitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameValuePair nvp = new NameValuePair(Environment.CommandLine);
+            string strFileName = nvp["FILE"];
+            string strChunkSize = nvp["CHUNKSIZE"];
+
+            if (strFileName == string.Empty)
+            {   // No filename specified
+                Console.WriteLine("ERROR: FileName not specified.");
+                Console.WriteLine("");
+                HowDoIUseThis();
+            }   // No filename specified
+            else
+            {   // Filename specified
+                if (nvp["ACTION"] == "COMBINE")
+                {   // Combine
+                    int CurrentFileIndexer = 1;
+
+                    using (System.IO.FileStream fileOutput = new System.IO.FileStream(strFileName, System.IO.FileMode.Create))
+                    {   // Output File Open
+                        while (true)
+                        {   // Infinite Loop
+                            string strInputFile = string.Format("{0}.part{1}", strFileName, CurrentFileIndexer);
+                            if (!System.IO.File.Exists(strInputFile))
+                            {   // File not found
+                                break;
+                            }   // File not found
+                            else
+                            {   // File found
+                                using (System.IO.FileStream fileInput = System.IO.File.OpenRead(strInputFile))
+                                {   // InputFile Open
+                                    while (fileInput.Position < fileInput.Length)
+                                    {   // Loop through all bytes in file
+                                        int byteval = fileInput.ReadByte();
+                                        if (byteval != -1)
+                                        {   // Not yet at end of file
+                                            fileOutput.WriteByte((byte)byteval);
+                                        }   // Not yet at end of file
+                                    }   // Loop through all bytes in file
+                                    fileInput.Close();
+                                }   // InputFile Open
+                            }   // File found.
+                            CurrentFileIndexer++;
+                        }   // Infinite loop
+                        fileOutput.Close();
+                    }   // Output File Open
+                }   // Combine
+                else
+                {   // Split
+                    if (!System.IO.File.Exists(strFileName))
+                    {
+                        Console.WriteLine("ERROR: Input file does not exist.");
+                        Console.WriteLine("");
+                        HowDoIUseThis();
+                    }
+                    else
+                    {   // Input File Exists
+                        long ChunkSize = GetBytes(strChunkSize);
+
+                        if (ChunkSize == -1)
+                        {   // Split by Unknown Chunk Size
+                            Console.WriteLine("ERROR: Chunksize invalid or not specified");
+                            Console.WriteLine("");
+                            HowDoIUseThis();
+                        }   // Split by Unknown Chunk Size
+                        else
+                        {   // ChunkSize specified
+                            System.IO.FileInfo fi = new System.IO.FileInfo(strFileName);
+                            using (System.IO.FileStream fileInput = fi.OpenRead())
+                            {   // Input file is open
+                                int CurrentFileIndexer = 1;
+                                while (fileInput.Position < fileInput.Length)
+                                {   // Stepping through the input file
+                                    string strOutputFile = string.Format("{0}.part{1}", strFileName, CurrentFileIndexer);
+                                    using (System.IO.FileStream fileOutput = System.IO.File.OpenWrite(strOutputFile))
+                                    {   // Output File is open
+                                        for (int i = 0; i < ChunkSize; i++)
+                                        {   // Step through byte by byte
+                                            int currentval = fileInput.ReadByte();
+                                            if (currentval == -1)
+                                            {   // Reached end of Input File
+                                                break;
+                                            }   // Reached end of Input File
+                                            fileOutput.WriteByte((byte)currentval);
+                                        }   // Step through byte by byte
+                                        fileOutput.Close();
+                                        CurrentFileIndexer++;
+                                    }   // Output File is open
+                                }   // Stepping through the input file
+                                fileInput.Close();
+                            }   // Input file is open
+                        }   // ChunkSize specified
+                    }   // Input File Exists
+                }   // Split
+            }   // Filename specified
+        }
+
+        /// <summary>
+        ///     Output to the screen how to use this program.
+        /// </summary>
+        static void HowDoIUseThis()
+        {
+            Console.WriteLine("Splitter.exe Action={Split|Combine} File={Filename} [ChunkSize={size}{units}]");
+            Console.WriteLine("     Action     What to do with the file(s)");
+            Console.WriteLine("                Split   - Take one large file and chop it into smaller chunks");
+            Console.WriteLine("                Combine - Take many smaller files and combine into one larger one");
+            Console.WriteLine("     FileName   The filename of the file being split or combined");
+            Console.WriteLine("     ChunkSize  How big each filepart should be (only applicable on Split)");
+            Console.WriteLine("         Size   How big");
+            Console.WriteLine("         Unit   What unit of measurement (case insensitive)");
+            Console.WriteLine("                   B         = Bytes");
+            Console.WriteLine("                   BYTES     = Bytes");
+            Console.WriteLine("                   K         = KiloBytes");
+            Console.WriteLine("                   KB        = KiloBytes");
+            Console.WriteLine("                   KILOBYTES = KiloBytes");
+            Console.WriteLine("                   M         = MegaBytes");
+            Console.WriteLine("                   MB        = MegaBytes");
+            Console.WriteLine("                   MEG       = MegaBytes");
+            Console.WriteLine("                   MEGS      = MegaBytes");
+            Console.WriteLine("                   MEGABYTES = MegaBytes");
+            Console.WriteLine("                   G         = GigaBytes");
+            Console.WriteLine("                   GB        = GigaBytes");
+            Console.WriteLine("                   GIG       = GigaBytes");
+            Console.WriteLine("                   GIGS      = GigaBytes");
+            Console.WriteLine("                   GIGABYTES = GigaBytes");
+        }
+
+        /// <summary>
+        ///     Get a long denoting how many bytes are represented by a given string
+        /// </summary>
+        /// <param name="ChunkSize">
+        ///     A string like:
+        ///         123kb
+        ///         1Meg
+        ///         10bytes
+        ///         etc.
+        /// </param>
+        /// <returns>
+        ///     How many bytes that represents.
+        /// </returns>
+        public static long GetBytes(string ChunkSize)
+        {   // GetBytes
+            long output = -1;
+            Regex r = new Regex("^([0-9]+)([a-zA-Z]+)");
+            Match mc = r.Match(ChunkSize);
+            if (mc.Groups.Count == 3)
+            {   // Scalar + Unit
+                long scalar;
+                long Multiplier = 1;
+                if (long.TryParse(mc.Groups[1].Value, out scalar))
+                {   // Scalar parsable
+                    switch (mc.Groups[2].Value)
+                    {   // Switch for the Unit
+                        case "K":
+                        case "KB":
+                        case "KILOBYTES":
+                            Multiplier = 1024;
+                            break;
+                        case "M":
+                        case "MB":
+                        case "MEG":
+                        case "MEGS":
+                        case "MEGABYTES":
+                            Multiplier = 1024 * 1024;
+                            break;
+                        case "G":
+                        case "GB":
+                        case "GIG":
+                        case "GIGS":
+                        case "GIGABYTES":
+                            Multiplier = 1024 * 1024 * 1024;
+                            break;
+                    }   // Switch for the Unit
+                    output = scalar * Multiplier;
+                }   // Scalar parsable
+            }   // Scalar + Unit
+            return output;
+        }   // GetBytes
+    }
+}
